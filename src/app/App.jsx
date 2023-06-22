783fb09fce7dfec729703a4c01723964b407c342
arseniyKokoApps <51684531+dalshenekuda@users.noreply.github.com>
2023-06-22 18:07:57 +0300

    format

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,7 @@
 import './scss/app.scss'
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Main from "../pages/main/Main";
 import Footer from "../widgets/footer/Footer";
 import Navigation from "../widgets/navigation/Navigation";
@@ -18,32 +18,32 @@ import Category from "../entities/category/ui/Category";
 
 const App = () => {
 
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  useEffect(() => {
-    dispatch(getCustomise())
-  }, [dispatch])
+    useEffect(() => {
+        dispatch(getCustomise())
+    }, [dispatch])
 
-  return (
-    <div className="wrapper">
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/portfolio/:id" element={<Project />} />
-          <Route path="/category" element={<CategoriesAll />} />
-          <Route path="/category/:id" element={<Category />} />
-          <Route path="/architect" element={<ArchitectsAll />} />
-          <Route path="/architect/:id" element={<Architect />} />
-          <Route path="/service/:id" element={<Service />} />
-          <Route path="/post/:id" element={<Post />} />
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <BrowserRouter>
+                <Navigation/>
+                <Routes>
+                    <Route path="/" element={<Main/>}/>
+                    <Route path="/portfolio" element={<Portfolio/>}/>
+                    <Route path="/portfolio/:id" element={<Project/>}/>
+                    <Route path="/category" element={<CategoriesAll/>}/>
+                    <Route path="/category/:id" element={<Category/>}/>
+                    <Route path="/architect" element={<ArchitectsAll/>}/>
+                    <Route path="/architect/:id" element={<Architect/>}/>
+                    <Route path="/service/:id" element={<Service/>}/>
+                    <Route path="/post/:id" element={<Post/>}/>
+                    <Route path="/blog" element={<Blog/>}/>
+                </Routes>
+                <Footer/>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
